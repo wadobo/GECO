8c0a0aa3a75b73b42513c64352bab3be60261a30
Daniel Garcia Moreno <dani@danigm.net>
2008-12-05 13:03:42 +0100

    Más ejemplos sobre sqlalchemy

---
+++ test_and_samples/sqlalchemy/tablas.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import *
+
+# Conexión a una base de datos
+db = create_engine('sqlite:///tutorial.db', echo=False)
+
+############ Definición de tablas ###############
+
+# Definición de un par de tablas
+metadata = MetaData()
+users = Table('users', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(50)),
+    Column('fullname', String(50)),
+)
+
+addresses = Table('addresses', metadata, 
+  Column('id', Integer, primary_key=True),
+  Column('user_id', None, ForeignKey('users.id', ondelete='CASCADE')),
+  Column('email_address', String(50), nullable=False)
+)
+
+from sqlalchemy.orm import mapper
+class User(object):
+    def __init__(self, name, fullname):
+        self.name = name
+        self.fullname = fullname
+
+    def __repr__(self):
+        return "<User('%s','%s')>" % (self.name, self.fullname)
+
+class Address(object):
+    def __init__(self, email_address, user_id):
+        self._username = user_id.name
+        self.user_id = user_id.id
+        self.email_address = email_address
+
+    def __repr__(self):
+        return "<Address('%s', '%s')>" % (self.email_address,
+                self._username)
+
+
+mapper(User, users)
+mapper(Address, addresses)
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=db, autoflush=True, transactional=True)
+session = Session()
+

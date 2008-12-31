1c9af6ddd272d9a1785bfc8087dfbeec9c178d05
danigm <dani@danigm.net>
2008-12-31 14:57:34 +0100

    Añadidos test del backend

--- src/unitests.py
+++ src/unitests.py
@@ -26,6 +26,28 @@ def testdb():
     assert u.passwords[0].name == 'mi pass'
     assert u.conffiles[0].name == 'mi file'
 
+def testbackend():
+    '''
+    backend
+    '''
+    import backend as b
+
+    b.register('dani2', 'pass')
+    assert b.check_user_name('dani2')
+
+    c = b.auth('dani2', 'password', password='pass')
+    b.set_password(c, 'nuevo', 'xxxx', type='email',
+            account='danigm', description="http://mail.danigm.net")
+    p = b.get_password(c, 'nuevo')
+    assert p.name == 'nuevo'
+
+    b.del_password(c, 'nuevo')
+    p = b.get_passwords_by(c, name='nuevo')
+    assert not list(p)
+
+    b.unregister(c)
+    assert not b.check_user_name('dani2')
+
 if __name__ == '__main__':
     # Se ejecutan todas las funciones que empiezan por test
     for k,v in globals().items():

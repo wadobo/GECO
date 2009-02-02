b4cb530b36e64b28f172ec9205dc6bc0d7fd341b
danigm <dani@danigm.net>
2009-02-02 21:15:01 +0100

    Cuando se pulsa sobre una contraseña esta se muestra durante 5 segundos para copiarla (launchpad #323517)
    https://bugs.launchpad.net/geco/+bug/323517

--- src/gecoc/web-geco/static/js/gecojs.js
+++ src/gecoc/web-geco/static/js/gecojs.js
@@ -1,10 +1,14 @@
 var mimasterpwd = "";
+var pwd = "";
+var timeout = 0;
 
 $(document).ready(function() {
+    link_to_forget();
+    setTimeout(link_to_forget, 2 * 1000);
+
     $(".pwdname").click(function(){
-        var pwd = $(this).next().html();
-        $(".input").fadeIn(300);
-        $("#overlay").fadeIn(300);
+        pwd = $(this).next().html();
+        show_passwd();
     });
     $(".close").click(function(){
         $(".input").fadeOut(300);
@@ -16,18 +20,72 @@ $(document).ready(function() {
         $("#masterpwd").attr("value", "");
         $(".input").fadeOut(300);
         $("#overlay").fadeOut(300);
-        setTimeout(olvidar, 10 * 60 * 1000);
+        setTimeout(forget, 10 * 60 * 1000);
+        really_show();
+    });
+    $("#forget").click(function(){
+        forget();
+    });
+    $("#freeze").click(function(){
+        if(timeout != 0){
+            clearTimeout(timeout);
+            timeout = 0;
+            $(this).html("continuar");
+        }
+        else{
+            n = parseInt($("#counter").html());
+            timeout = setTimeout('pass_delete('+(n-1)+')', 1 * 1000);
+            $(this).html("detener");
+        }
+        $("#clear").focus();
+        $("#clear").select();
     });
 });
 
-function alertar(){
-    alert(mimasterpwd);
+function link_to_forget(){
+    if(mimasterpwd != "")
+        $("#forget").show();
+    else
+        $("#forget").hide();
+    setTimeout(link_to_forget, 2 * 1000);
 }
 
-function olvidar(){
+function show_passwd(){
+    if(mimasterpwd == ""){
+        $(".input").fadeIn(300);
+        $("#masterpwd").select();
+        $("#overlay").fadeIn(300);
+    }
+    else {
+        really_show();
+    }
+}
+
+function pass_delete(n){
+    $("#counter").html(n);
+    if(n > 0)
+        timeout = setTimeout('pass_delete('+(n-1)+')', 1 * 1000);
+    else{
+        $("#clear").attr("value", "");
+        $("#counter").hide();
+    }
+}
+
+function really_show(){
+    var pass = decrypt(mimasterpwd, pwd);
+    $("#clear").attr("value", pass);
+    $("#clear").focus();
+    $("#clear").select();
+    $("#counter").show();
+    timeout = setTimeout('pass_delete(5)', 1 * 1000);
+    
+    pass = "";
+}
+
+function forget(){
     mimasterpwd = "";
+    pwd = "";
 }
-
 
 function array_of_bytes(key){
     var retkey = new Array();
@@ -81,7 +139,8 @@ function encrypt(key, clear){
 function decrypt(key, cypher){
     var realkey = getkey(key);
     var realiv = getiv(key);
+    var realcypher = cypher.split('#');
 
-    var value = slowAES.decrypt(cypher, 256, slowAES.modeOfOperation.CBC, realkey, 16, realiv);
+    var value = slowAES.decrypt(realcypher, 256, slowAES.modeOfOperation.CBC, realkey, 16, realiv);
     return array_to_string(value);
 };

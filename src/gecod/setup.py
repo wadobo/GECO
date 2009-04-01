3844ba6f22e5894f8233d2684ceec8cda1e75ba1
Daniel Garcia Moreno <dani@danigm.net>
2009-04-01 14:07:00 +0200

    setup.py completo, creo que funciona :P

--- src/gecod/setup.py
+++ src/gecod/setup.py
@@ -5,23 +5,38 @@ import shutil
 
 datafiles = []
 
-#TODO los datos no se instalan bien porque no existen los directorios
+prefix = '/usr'
+for arg in sys.argv:
+    if arg.startswith('--prefix') or\
+            arg.startswith('--home'):
+        opt, prefix = arg.split('=')
 
 response = raw_input('Crear base de datos [Y/n]: ')
 if not response or response.lower() in ['Yy']:
     from gecod import database
     database.create()
-    datafiles.append(('gecod/', 'database.sqlite'))
+    datafiles.append(('share/gecod/', ['database.sqlite']))
 
 response = raw_input('Generar certificados [Y/n]: ')
 if not response or response.lower() in ['Yy']:
     os.chdir('certs')
     os.popen('bash generate-pem.sh')
-    datafiles.append(('gecod/certs/',
-        ['certs/cert.pem', 'certs/key.pem']))
+    os.chdir('..')
+    datafiles.append(('share/gecod/certs/', ['certs/cert.pem', 'certs/key.pem']))
 
-#if not os.path.exists('/etc/gecod-xmlrpc.conf'):
-#    shutil.copy('gecod-xmlrpc.conf.example', '/etc/gecod-xmlrpc.conf')
+conffile = '''host = localhost
+port = 4343
+database = sqlite://%(path)s/database.sqlite
+KEYFILE = %(path)s/certs/key.pem
+CERTFILE = %(path)s/certs/cert.pem
+''' % {'path': prefix+'/share/gecod'}
+
+conf = open('gecod-xmlrpc.conf', 'w')
+conf.write(conffile)
+conf.close()
+
+if not os.path.exists('/etc/gecod-xmlrpc.conf'):
+    datafiles.append(('/etc/gecod-xmlrpc.conf', ['gecod-xmlrpc.conf']))
 
 setup(name = 'gecod-xmlrpc',
       version = '1.0',

0197f6bd4400f2d681f4167f33d9e325002fea4f
root <root@Stibbons>
2009-05-21 16:03:52 +0000

    Empezando el paquete debian

--- src/gecod/setup.py
+++ src/gecod/setup.py
@@ -4,24 +4,28 @@ import sys
 
 datafiles = []
 
+ask = lambda x: 'N'
 prefix = '/usr'
+
 for arg in sys.argv:
     if arg.startswith('--prefix') or\
             arg.startswith('--home'):
         opt, prefix = arg.split('=')
+    elif arg.startswith('--ask'):
+        ask = raw_input
 
-response = raw_input('Crear base de datos [Y/n]: ')
-if not response or response.lower() in 'Yy':
-    from gecod import database
-    database.create()
-    datafiles.append(('share/gecod/', ['database.sqlite']))
-
-response = raw_input('Generar certificados [Y/n]: ')
-if not response or response.lower() in 'Yy':
-    os.chdir('certs')
-    os.popen('bash generate-pem.sh')
-    os.chdir('..')
-    datafiles.append(('share/gecod/certs/', ['certs/cert.pem', 'certs/key.pem']))
+#response = ask('Crear base de datos [Y/n]: ')
+#if not response or response.lower() in 'Yy':
+#    from gecod import database
+#    database.create()
+#    datafiles.append(('share/gecod/', ['database.sqlite']))
+#
+#response = ask('Generar certificados [Y/n]: ')
+#if not response or response.lower() in 'Yy':
+#    os.chdir('certs')
+#    os.popen('bash generate-pem.sh')
+#    os.chdir('..')
+#    datafiles.append(('share/gecod/certs/', ['certs/cert.pem', 'certs/key.pem']))
 
 conffile = '''host = localhost
 port = 4343
@@ -34,10 +38,8 @@ conf = open('gecod-xmlrpc.conf', 'w')
 conf.write(conffile)
 conf.close()
 
-if not os.path.exists('/etc/gecod-xmlrpc.conf'):
-    datafiles.append(('/etc', ['gecod-xmlrpc.conf']))
-
-# TODO Anadir un demonio en /etc/init.d
+if not os.path.exists(prefix+'/../etc/gecod-xmlrpc.conf'):
+    datafiles.append((prefix+'/../etc', ['gecod-xmlrpc.conf']))
 
 setup(name = 'gecod-xmlrpc',
       version = '1.0',
